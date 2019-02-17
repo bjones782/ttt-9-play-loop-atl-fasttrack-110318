36d9ead6d62302a86db3bf76f88de23fd69fb3a0
Brooke Jones <bjones782@gmail.com>
2019-02-17 18:31:55 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -46,12 +46,3 @@ def turn_count(board)
   return turn
 end
 
-def turn_count(board)
-  turn = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
-  end
-  return turn
-end
